1b001e17beda3420e5c2da8eac86d29b62b21e9e
Rita Kaufman <rita1kaufman@gmail.com>
2019-02-15 23:25:12 -0500

    added names

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var config = require('./config');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var connectedUsers = {};
+var connectedUsers = [];
 
 var port = process.env.PORT || 3000;
 
@@ -21,21 +21,34 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket){
     console.log('A user is connected');
+
+    //Disconnecting form the server
     socket.on('disconnect', function(){
-        console.log('user disconnected');
+        var username = socket.username || "";
+        if(connectedUsers.length > 0 && username != ""){
+            connectedUsers.splice(connectedUsers.indexOf(username), 1);
+        }
+        
+        console.log('User ' + username + ' disconnected');
       });
-    //saving a reference to this user's socket
-    //connectedUsers[curruser]=socket;
+
+    //Sending a message
     socket.on('chat message', function(data){
         console.log('message: ' + data.message);
-        //connectedUsers[selectedUser].emit('chat message', {message: msg});
-        io.emit('chat message', {message: data.message});
+        io.emit('new message', {message: data.message, username: socket.username});
     });
+
+    //New user
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        connectedUsers.push(socket.username);
+    })
 });
 
 mongoose.connect(config.getDbConnectionString(), { useNewUrlParser: true });
 
-var server = http.listen(port, function(){
+var server = http.listen(port , function(){
     console.log('Server is running on port ' + server.address().port);
 })
 
