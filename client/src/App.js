bd34371ab8a79a2e938a1fdf99db093db950f0d9
Rita Kaufman <rita1kaufman@gmail.com>
2019-02-28 14:41:02 -0500

    Added basic screen managing

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Chat from './containers/Chat.js';
+import Login from './containers/Login';
+import Registration from './containers/Registration';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loginScreen: true,
+      registrationScreen: false,
+      chatScreen: false
+    }
+  }
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
         </header>
+        {this.state.loginScreen && <Login />}
+        {this.state.registrationScreen && <Registration />}
+        {this.state.chatScreen && <Chat />}
       </div>
     );
   }
