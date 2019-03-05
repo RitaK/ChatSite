99ddc84f529f5c45026e6ee4d62e6a4c1aa8468b
Rita Kaufman <rita1kaufman@gmail.com>
2019-03-05 18:09:39 -0500

    changed main theme styles a bit

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import {Button, TextField, Paper} from '@material-ui/core';
+import {Button, TextField, Paper, Grid} from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
+import Lock from '@material-ui/icons/Lock';
 
 const styles = theme => ({
     root: {
-      textAlign: 'center',
+      textAlign: 'center'
       //paddingTop: theme.spacing.unit * 14,
     },
     Paper: {
@@ -20,29 +21,64 @@ const styles = theme => ({
     },
     Button: {
         'margin-top': theme.spacing.unit * 2
+    },
+    Typography: {
+        width: '100%'
+    },
+    Lock: {
+        width: '100%',
+        'margin-bottom': theme.spacing.unit
     }
   });
 
 class Login extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            username: 'username',
+            password: 'password'
+        }
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    }
+
+    handleUsernameChange(event){
+        this.setState({username: event.target.value})
+    }
+
+    handlePasswordChange(event){
+        this.setState({password: event.target.value})
+    }
+    
     render(){
         const {classes} = this.props;
         return(
             <div>
                 <Paper className = {classes.Paper} elevation = {4}>
-                    <Typography component="h4" variant="display2" gutterBottom>
-                    Login page
-                    </Typography>
-                    <TextField className = {classes.TextField} value={"bla"} fullWidth= {true}>
-                        
-                    </TextField>
-                    <TextField className = {classes.TextField} value={"bla"} fullWidth= {true}>
-                        
-                    </TextField>
-                    <Button className = {classes.Button} variant="contained" color="primary" fullWidth = {true}>
+                    <Grid container>
+                        <Lock className = {classes.Lock} color = "primary"/>
+                        <Typography className={classes.Typography} component="title" variant="h5" gutterBottom>
                         Login
-                    </Button>
+                        </Typography>
+                        <TextField className = {classes.TextField} 
+                        fullWidth= {true} onChange={this.handleUsernameChange}
+                        label="User Name" />
+                        <TextField className = {classes.TextField} 
+                        fullWidth= {true} onChange={this.handlePasswordChange}
+                        label= "Password" type="Password" autoComplete="current-password"/>
+                        <Grid item  sm= {4}>
+                            <Button className = {classes.Button} variant="contained" color="primary" fullWidth = {true}>
+                                Login
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button className = {classes.Button} variant="contained" color="primary" fullWidth = {true}>
+                                Create an account
+                            </Button>
+                        </Grid>
+                    </Grid>
                 </Paper>
-            </div>
+            </div> 
         );
     }
 }
--- client/src/withRoot.js
+++ client/src/withRoot.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
+import pink from '@material-ui/core/colors/pink';
 import green from '@material-ui/core/colors/green';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 const theme = createMuiTheme({
-    
-    overrides: {
         palette: {
-            primary:  { main: purple[500] },
+            primary:  {main:pink['A100']},
             secondary: green,
           },
-        /* MuiButton: {
-            label:{
-                background: '#18202c'
-            },
-        }, */
-    },
+        typography: {
+            useNextVariants: true,
+          }
   });
   
 
